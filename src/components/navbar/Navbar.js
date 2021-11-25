3edbf67857ef0fc952bef0fe7256abb52301b54e
DanteBorges <danielnorges@gmail.com>
2021-11-25 17:42:00 -0300

    css changes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -44,11 +44,7 @@ const Navbar = () => {
         </div>
       </nav>
 
-      {/* hero section  */}
-      {/* <section className="hero-section">
-        <p>Welcome to </p>
-        <h1>Thapa Technical</h1>
-      </section> */}
+
     </>
   );
 };
